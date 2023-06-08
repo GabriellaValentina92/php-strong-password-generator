f3425dc2de52d5b0bd28fd2f4b98d88ed74ee690
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-06-08 18:41:01 +0200

    Add only logic file

---
+++ logic.php
@@ -0,0 +1,15 @@
+<?php 
+    $passLength = $_GET['pass_number'];
+    $letters = range('a', 'z');
+    $numbers = [1, 3, 5, 7, 9, 2, 4, 12, 14, 92, 33, 48, 66, 208, 10];
+    $symbols = ['@', '_', '&', '#', '*', '-', '/',];
+
+    function GeneratePassword($passLength, $letters, $numbers, $symbols){
+        if($passLength){
+            $arrMerged = array_merge($letters, $numbers, $symbols);
+            shuffle($arrMerged);
+            $arrToString = implode($arrMerged);
+            return substr($arrToString, $passLength); 
+        };
+    }
+?>
